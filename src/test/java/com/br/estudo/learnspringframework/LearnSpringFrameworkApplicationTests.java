bc54de53fdb6717092a18edef3aa12f674bdda7f
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-03-26 14:52:27 -0300

    feat: add Spring Web dependency

--- src/test/java/com/br/estudo/learnspringframework/LearnSpringFrameworkApplicationTests.java
+++ src/test/java/com/br/estudo/learnspringframework/LearnSpringFrameworkApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LearnSpringFrameworkApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
