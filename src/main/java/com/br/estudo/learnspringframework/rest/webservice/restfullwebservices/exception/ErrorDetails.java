9d6e01d66efa9b862704ef35633e8c5dcf9bf34d
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-03-27 14:36:52 -0300

    feat: Implementing Generic Exception Handling for all Resources

---
+++ src/main/java/com/br/estudo/learnspringframework/rest/webservice/restfullwebservices/exception/ErrorDetails.java
@@ -0,0 +1,32 @@
+package com.br.estudo.learnspringframework.rest.webservice.restfullwebservices.exception;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class ErrorDetails {
+
+    //timestamp
+    private LocalDateTime timestamp;
+    //message
+    private String message;
+    //details
+    private String details;
+
+    public ErrorDetails(LocalDateTime timestamp, String message, String details) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+}
