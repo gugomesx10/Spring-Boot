575714bc81486616c2d3926f7bc2e03289c06024
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-03-27 16:36:22 -0300

    feat: feat: implement API versioning for person controller with working GET endpoint

--- src/main/java/com/br/estudo/learnspringframework/rest/webservice/restfullwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/br/estudo/learnspringframework/rest/webservice/restfullwebservices/versioning/VersioningPersonController.java
@@ -1,4 +1,15 @@
 package com.br.estudo.learnspringframework.rest.webservice.restfullwebservices.versioning;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class VersioningPersonController {
+
+    @GetMapping("/v1/person")
+    public PersonV1 getFirstVersionOfPerson(){
+        return new PersonV1("Pedro");
+    }
 }
