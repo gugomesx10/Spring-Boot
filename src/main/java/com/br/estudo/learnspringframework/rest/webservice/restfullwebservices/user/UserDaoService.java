0e14a2425e93e75a2027f0c5dcec61c17dbc1d3f
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-03-27 14:43:21 -0300

    feat: Implementing DELETE Method to delete a User Resource

--- src/main/java/com/br/estudo/learnspringframework/rest/webservice/restfullwebservices/user/UserDaoService.java
+++ src/main/java/com/br/estudo/learnspringframework/rest/webservice/restfullwebservices/user/UserDaoService.java
@@ -42,6 +42,12 @@ public class UserDaoService {
                 .orElse(null);
     }
 
+    public void deleteById(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        users.removeIf(predicate);
+
+    }
+
 
 
 }
--- src/main/java/com/br/estudo/learnspringframework/rest/webservice/restfullwebservices/user/UserResource.java
+++ src/main/java/com/br/estudo/learnspringframework/rest/webservice/restfullwebservices/user/UserResource.java
@@ -34,6 +34,11 @@ public class UserResource {
         return user;
     }
 
+    @DeleteMapping("/users/{id}")
+    public void deleteUser (@PathVariable int id) {
+        service.deleteById(id);
+    }
+
     //POST /users
     @PostMapping("/users")
     public ResponseEntity<User> createUser(@RequestBody User user) {
