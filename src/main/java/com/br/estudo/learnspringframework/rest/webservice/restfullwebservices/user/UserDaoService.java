25aae40ab8196f14e61a383611a832fed699d1a1
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-03-27 13:21:25 -0300

    feat: Implementing POST method to create user resource

--- src/main/java/com/br/estudo/learnspringframework/rest/webservice/restfullwebservices/user/UserDaoService.java
+++ src/main/java/com/br/estudo/learnspringframework/rest/webservice/restfullwebservices/user/UserDaoService.java
@@ -14,17 +14,24 @@ public class UserDaoService {
 
     private static List<User> users = new ArrayList<>();
 
+    private static int usersCount = 0;
+
     static {
-        users.add(new User(1, "Gustavo", LocalDate.now().minusYears(23)));
-        users.add(new User(2, "Giselen", LocalDate.now().minusYears(22)));
-        users.add(new User(3, "Cecilia", LocalDate.now().minusYears(50)));
+        users.add(new User(++usersCount, "Gustavo", LocalDate.now().minusYears(23)));
+        users.add(new User(++usersCount, "Giselen", LocalDate.now().minusYears(22)));
+        users.add(new User(++usersCount, "Cecilia", LocalDate.now().minusYears(50)));
     }
+
 
     public List<User> findAll() {
         return users;
     }
 
-    //public User save(User user) {
+    public User save(User user) {
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
+    }
 
     public User findOne(int id) {
         Predicate<? super User> predicate = user -> user.getId().equals(id);
--- src/main/java/com/br/estudo/learnspringframework/rest/webservice/restfullwebservices/user/UserResource.java
+++ src/main/java/com/br/estudo/learnspringframework/rest/webservice/restfullwebservices/user/UserResource.java
@@ -1,9 +1,6 @@
 package com.br.estudo.learnspringframework.rest.webservice.restfullwebservices.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +23,11 @@ public class UserResource {
     @GetMapping("/users/{id}")
     public User retrieveUsers(@PathVariable int id) {
         return service.findOne(id);
+    }
+
+    //POST /users
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {
+        service.save(user);
     }
 }
